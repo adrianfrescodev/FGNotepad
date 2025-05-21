ad7b4f85db303499b540f418b99c8712ce017bb9
Ninjaadrian009 <frescoadrian5000@gmail.com>
2025-05-21 17:58:20 -0400

    remade ui and added mobile support

--- public/script.js
+++ public/script.js
@@ -29,39 +29,38 @@ const db = getFirestore(app);
 let currentuser = null;
 let logout = document.getElementById("logoutbutton");
 let login = document.getElementById("loginbutton");
-
-login.addEventListener('click', () =>{
-    signInWithPopup(auth, provider)
-  .then((result) => {
-})
+const data = {}
+login.addEventListener('click', () => {
+  signInWithPopup(auth, provider)
+    .then((result) => {
+    })
     .catch((error) => {
-    alert("Login failed");
+      alert("Login failed");
     }
-)
+    )
 })
-logout.addEventListener('click', () =>
-{
-    signOut(auth).then(() => {
+logout.addEventListener('click', () => {
+  signOut(auth).then(() => {
     location.reload();
-})
+  })
 }
 )
-async function setnotes(userid){
-    const docRef = doc(db, "users", userid);
-    await setDoc(docRef, data);
+async function setnotes(userid) {
+  const docRef = doc(db, "users", userid);
+  await setDoc(docRef, data);
 }
-async function loadnotes(userid){
-    const docRef = doc(db, "users", userid);
-    const docSnap = await getDoc(docRef);
-    if (docSnap.exists()) {
+async function loadnotes(userid) {
+  const docRef = doc(db, "users", userid);
+  const docSnap = await getDoc(docRef);
+  if (docSnap.exists()) {
     Object.assign(data, docSnap.data());
-} else {
+  } else {
 
-  console.log("No such document!");
-}
+    console.log("No such document!");
+  }
 }
 
-onAuthStateChanged (auth, async (user) => {
+onAuthStateChanged(auth, async (user) => {
   if (user) {
     logout.classList.remove("hidden");
     login.classList.add("hidden");
@@ -76,87 +75,87 @@ onAuthStateChanged (auth, async (user) => {
 let homepage = document.getElementById("home");
 let characterScreen = document.getElementById("char-screen");
 let currentTab = "general";
-let currentCharacterDisplay = document.getElementById("currentchar");
+let currentCharacterDisplayPc = document.getElementById("currentcharpc");
+let currentCharacterDisplayMobile = document.getElementById("currentcharmobile");
 let currentCharacter = "";
 let currentCharacterPicture = document.getElementById("currentpic");
 const userNotes = document.getElementById("user-notes");
 const selects = document.querySelectorAll("input[name='character']");
-const data = {}
-selects.forEach( selection => {
-    selection.addEventListener('click', (e) =>{
+selects.forEach(selection => {
+  selection.addEventListener('click', (e) => {
     loadChar(e.target);
-    })
+  })
 })
 const tabmap = {
-    general: "General notes",
-    "key-moves": "Key moves",
-    combos: "Combos"
+  general: "General notes",
+  "key-moves": "Key moves",
+  combos: "Combos"
 }
 const tabs = document.querySelectorAll("input[name='tab']");
-tabs.forEach( tab => {
-    tab.addEventListener('click', (e) =>{
-        switchTab(e.target);
-    })
+tabs.forEach(tab => {
+  tab.addEventListener('click', (e) => {
+    switchTab(e.target);
+  })
 })
-async function goHome(){
-    if (currentCharacter) {
-        data[currentCharacter][currentTab] = userNotes.value;
-         if (currentuser){
-            await setnotes(currentuser.uid);
-        } 
+async function goHome() {
+  if (currentCharacter) {
+    data[currentCharacter][currentTab] = userNotes.value;
+    if (currentuser) {
+      await setnotes(currentuser.uid);
     }
-    currentCharacter = "";
-    homepage.classList.remove("hidden");
-    characterScreen.classList.add("hidden");
-    document.querySelectorAll("input[name='tab']").forEach(tab => {
+  }
+  currentCharacter = "";
+  homepage.classList.remove("hidden");
+  characterScreen.classList.add("hidden");
+  document.querySelectorAll("input[name='tab']").forEach(tab => {
     tab.checked = false;
-    
-})
+
+  })
 }
 const banner = document.getElementById("banner")
-banner.addEventListener("click", async () =>{
-    await goHome()
+banner.addEventListener("click", async () => {
+  await goHome()
 })
-function loadChar(newChar){
-    if(currentCharacter !== ""){
-        data[currentCharacter][currentTab] = userNotes.value;
-    }
-    currentCharacter = newChar.value;
-    if(!data[newChar.value]) {
-    data[newChar.value] = {
-    general: "",
-    "key-moves": "",
-    combos: ""
-  }
-}
-    currentTab = "general";
-    document.getElementById("general").checked = true;
-    userNotes.placeholder = tabmap[currentTab];
-    userNotes.value = data[currentCharacter][currentTab];
-    currentCharacterDisplay.textContent = newChar.value;
-    currentCharacterPicture.src = "Images/" + newChar.value + "-full.png";
-    homepage.classList.add("hidden");
-    characterScreen.classList.remove("hidden");
-}
-async function switchTab(newtab){
-     if(currentuser)
-        {
-        await setnotes(currentuser.uid);
-    }
+function loadChar(newChar) {
+  if (currentCharacter !== "") {
     data[currentCharacter][currentTab] = userNotes.value;
-    currentTab = newtab.value;
-    userNotes.placeholder = tabmap[newtab.value];
-    userNotes.value = data[currentCharacter][currentTab];
+  }
+  currentCharacter = newChar.value;
+  if (!data[newChar.value]) {
+    data[newChar.value] = {
+      general: "",
+      "key-moves": "",
+      combos: ""
+    }
+  }
+  currentTab = "general";
+  document.getElementById("general").checked = true;
+  userNotes.placeholder = tabmap[currentTab];
+  userNotes.value = data[currentCharacter][currentTab];
+  currentCharacterDisplayPc.textContent = newChar.value;
+  currentCharacterDisplayMobile.textContent = newChar.value;
+  currentCharacterPicture.src = "Images/" + newChar.value + "-full.png";
+  homepage.classList.add("hidden");
+  characterScreen.classList.remove("hidden");
 }
-document.addEventListener("beforeunload",() => {
-  if(currentuser){
+async function switchTab(newtab) {
+  if (currentuser) {
+    await setnotes(currentuser.uid);
+  }
+  data[currentCharacter][currentTab] = userNotes.value;
+  currentTab = newtab.value;
+  userNotes.placeholder = tabmap[newtab.value];
+  userNotes.value = data[currentCharacter][currentTab];
+}
+document.addEventListener("beforeunload", () => {
+  if (currentuser) {
     setnotes(currentuser.uid);
   }
-   
+
 })
 
 document.addEventListener("visibilitychange", async () => {
   if (document.visibilityState === "hidden" && currentuser) {
     await setnotes(currentuser.uid);
-  } 
+  }
 });
